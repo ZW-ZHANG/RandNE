95df842cf0959b8f75a930849ff136b1208aeb96
ZW-ZHANG <zw-zhang16@mails.tsinghua.edu.cn>
2018-10-18 11:22:17 +0800

    Update Sample_Run

--- Sample_Run.m
+++ Sample_Run.m
@@ -2,21 +2,22 @@
 % load data
 load('BlogCatalog');
 
-% Parameters
-q = 2;
+% Common parameters
 d = 128;
 Ortho = 1;
 seed = 0;
-weights = [1,0.1,0.001];
 
-% embedding for adjacency matrix
+% embedding for adjacency matrix for reconstruction
+q = 2;
+weights = [1,0.1,0.001];
 U_list = RandNE_Projection(A,q,d,Ortho,seed);
 U = RandNE_Combine(U_list,weights);
-% reconstruction
 prec = Precision_Np(A,sparse(length(A),length(A)),U,U,1e6);
 semilogx(1:1e6,prec);
 
-% embedding for transition matrix
-A_tran = spdiags(1 ./ sum(A,2),0,length(A),length(A));
+% embedding for transition matrix for classification
+q = 3;
+weights = [1,1e2,1e4,1e5];
+A_tran = spdiags(1 ./ sum(A,2),0,length(A),length(A)) * A;
 U_list = RandNE_Projection(A_tran,q,d,Ortho,seed);
 U = RandNE_Combine(U_list,weights);
